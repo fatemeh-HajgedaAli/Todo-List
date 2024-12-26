68bfbc6997eb2f4ef7c4456a66b3f657163725fb
Fatemeh HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2024-12-26 10:41:18 +0330

    finish deBaging

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
 let $=document
-
-let itemList=$.querySelector('.itemList')
+let parentMenu=$.querySelector('ul')
 let trashBox=$.querySelector('.trashBox')
 let text=$.querySelector('.text')
-let listMenu=$.querySelectorAll('.listMenu')
 let textInput=$.querySelector('#textInput')
 let formBox=$.querySelector('.formBox')
 
+function addNewTodo(textInputValue){
 
-function addNewTodo(){
-
-let newItemList=$.createElement('li')
-newItemList.className='itemList'
-
-let newText=$.createElement('p')
-newText.innerHTML=textInputValue
+let newChildList=$.createElement('li')
+newChildList.className='itemList'
+newChildList.innerHTML=textInputValue
 
 let newTrashIcon=$.createElement('i')
-newTrashIcon.className='bi bi-trash3-fill trashBox'
+newTrashIcon.className='bi bi-trash-fill trashBox'
 
+// event on trash box for removing
 newTrashIcon.addEventListener('click',function(event){
 event.target.parentElement.remove()
-
-newItemList.append(newText , newTrashIcon)
-listMenu[0].append(newItemList)
 })
+
+newChildList.append(newTrashIcon);
+parentMenu.append(newChildList);
+
 }
+textInput.addEventListener('keydown',function(event){  
+
+let textInputValue=textInput.value.trim()
+
+parentMenu.style.marginTop='-0.2em'
+parentMenu.style.display='block'
+if(textInputValue){
+if(event.keyCode===13){
+textInput.value=''
+addNewTodo(textInputValue)
+}
+}
+})
+
+// submit and preventDefault
 formBox.addEventListener('submit', function(event){
     event.preventDefault()
 })
-textInput.addEventListener('keydown',function(event){  
-
-textInputValue=textInput.value.trim()
-listMenu[0].style.marginTop='-0.2'
-listMenu[0].style.display='inline'
-
-if(event.keyCode === 13 ){
-    if(textInputValue){
-textInput.value=''
-addNewTodo(textInputValue)
-
-}
-}
-})
-
 
