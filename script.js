3a28a26959acb689ed8ec033449ba236a0cedaef
Fatemeh HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2024-12-26 11:28:18 +0330

    coding by HTML/CSS/JS

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ let trashBox=$.querySelector('.trashBox')
 let text=$.querySelector('.text')
 let textInput=$.querySelector('#textInput')
 let formBox=$.querySelector('.formBox')
-
+// 
 function addNewTodo(textInputValue){
-
+// make new li
 let newChildList=$.createElement('li')
 newChildList.className='itemList'
 newChildList.innerHTML=textInputValue
 
+// male new trashIcon
 let newTrashIcon=$.createElement('i')
 newTrashIcon.className='bi bi-trash-fill trashBox'
 
@@ -18,16 +19,15 @@ newTrashIcon.className='bi bi-trash-fill trashBox'
 newTrashIcon.addEventListener('click',function(event){
 event.target.parentElement.remove()
 })
-
+// appending element
 newChildList.append(newTrashIcon);
 parentMenu.append(newChildList);
 
 }
+// event for getting Input Value and make new to-do-list
+
 textInput.addEventListener('keydown',function(event){  
-
 let textInputValue=textInput.value.trim()
-
-parentMenu.style.marginTop='-0.2em'
 parentMenu.style.display='block'
 if(textInputValue){
 if(event.keyCode===13){
@@ -39,6 +39,6 @@ addNewTodo(textInputValue)
 
 // submit and preventDefault
 formBox.addEventListener('submit', function(event){
-    event.preventDefault()
+ event.preventDefault()
 })
 
